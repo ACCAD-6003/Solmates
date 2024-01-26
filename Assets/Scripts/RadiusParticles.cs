67043400a59d0da2354636252ae860a858112b71
Chaseao <oberg.17@buckeyemail.osu.edu>
2024-01-26 13:51:19 -0500

    Particle System added

---
+++ Assets/Scripts/RadiusParticles.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class RadiusParticles : MonoBehaviour
+{
+    private readonly float scalingFactor = 40f;
+    
+    [SerializeField] private Movement2 player;
+
+    private ParticleSystem particles;
+    
+    private void Awake()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+    
+    private void OnEnable()
+    {
+        StartCoroutine(KeepRadiusUpdated());
+    }
+    
+    private IEnumerator KeepRadiusUpdated()
+    {
+        while (true)
+        {
+            var lastRadius = player.RadiusVector().magnitude;
+            
+            var shape = particles.shape;
+            shape.radius = lastRadius;
+
+            var emissionRate = particles.emission;
+            emissionRate.rateOverTime = lastRadius * scalingFactor;
+            
+            yield return new WaitUntil(() => Math.Abs(lastRadius - player.RadiusVector().magnitude) > float.Epsilon);
+        }
+    }
+}
