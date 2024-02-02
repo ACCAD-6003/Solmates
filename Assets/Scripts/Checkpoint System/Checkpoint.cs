c8fc3900db361f36d3e0900a261118dc4cc4d634
Chaseao <oberg.17@buckeyemail.osu.edu>
2024-02-02 17:08:47 -0500

    Added ability for checkpoint dialogue and intro

--- Assets/Scripts/Checkpoint System/Checkpoint.cs
+++ Assets/Scripts/Checkpoint System/Checkpoint.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using UI.Dialogue_System;
 using UnityEngine;
 
 namespace Checkpoint_System
@@ -8,11 +9,13 @@ namespace Checkpoint_System
     {
         [SerializeField] private Vector3 pointToRespawn;
         [SerializeField] private float rotation;
+        [SerializeField] private SOConversationData conversation;
         
         public static Action<Checkpoint> OnCheckpointReached;
 
         public Vector3 PointToRespawn => pointToRespawn;
         public float Rotation => rotation;
+        public SOConversationData Conversation => conversation;
         
         private readonly List<Collider> playersWhoReachedZone = new(); 
 
