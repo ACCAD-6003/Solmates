7822c14c87fc4638e13026ad9f0c8cdbd03d7580
Chaseao <oberg.17@buckeyemail.osu.edu>
2024-02-06 08:28:46 -0500

    Added End Zones

--- Assets/Scripts/Checkpoint System/Checkpoint.cs
+++ Assets/Scripts/Checkpoint System/Checkpoint.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UI.Dialogue_System;
 using UnityEngine;
 
@@ -7,18 +8,27 @@ namespace Checkpoint_System
 {
     public class Checkpoint : MonoBehaviour
     {
-        [SerializeField] private Vector3 pointToRespawn;
-        [SerializeField] private float rotation;
-        [SerializeField] private SOConversationData conversation;
-        
+        [SerializeField] private bool endSpot;
+        [SerializeField, ShowIf("@!endSpot")] private Vector3 pointToRespawn;
+        [SerializeField, ShowIf("@!endSpot")] private float rotation;
+        [SerializeField, ShowIf("@!endSpot")] private SOConversationData conversation;
+
         public static Action<Checkpoint> OnCheckpointReached;
 
         public Vector3 PointToRespawn => pointToRespawn;
         public float Rotation => rotation;
         public SOConversationData Conversation => conversation;
         
-        private readonly List<Collider> playersWhoReachedZone = new(); 
+        private readonly List<Collider> playersWhoReachedZone = new();
 
+        [Button("DEBUG Jump To Point"), ShowIf("@!endSpot")]
+        private void DebugJumpToPoint()
+        {
+            playersWhoReachedZone.Add(null);
+            playersWhoReachedZone.Add(null);
+            OnCheckpointReached?.Invoke(this);
+        }
+        
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player") && !playersWhoReachedZone.Contains(other))
@@ -26,7 +36,14 @@ namespace Checkpoint_System
                 playersWhoReachedZone.Add(other);
                 if(playersWhoReachedZone.Count == 2)
                 {
-                    OnCheckpointReached?.Invoke(this);
+                    if (endSpot)
+                    {
+                        StartCoroutine(SceneTools.TransitionToScene(SceneTools.NextSceneIndex));
+                    }
+                    else
+                    {
+                        OnCheckpointReached?.Invoke(this);
+                    }
                 }
             }
         }
