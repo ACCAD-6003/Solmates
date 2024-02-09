7c73d85583af1f9c3b9208c2e7b5713935a9520d
Chaseao <oberg.17@buckeyemail.osu.edu>
2024-02-09 13:18:18 -0500

    Added two monitors

--- Assets/Scripts/DisplayStarter.cs
+++ Assets/Scripts/DisplayStarter.cs
@@ -1,17 +1,41 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DisplayStarter : SingletonMonoBehavior<DisplayStarter>
 {
-    private bool secondDisplayFound;
+    [SerializeField] private bool debugMode;
     
+    private static bool SecondDisplayFound => Display.displays.Length > 1;
+
     private void Start()
     {
         Display.onDisplaysUpdated += ToggleDisplay;
-        Display.displays[0].Activate();
+        SceneManager.sceneLoaded += (_, _) => ToggleDisplay();
+        ToggleDisplay();
     }
 
+    [Button("DEBUG Refresh Display")]
     private void ToggleDisplay()
     {
-        Debug.Log("Screem");
+        var blueCamera = GameObject.FindGameObjectWithTag("BlueCamera").GetComponent<Camera>();
+        var redCamera = GameObject.FindGameObjectWithTag("RedCamera").GetComponent<Camera>();
+
+        blueCamera.targetDisplay = 0;
+        Debug.Log(SecondDisplayFound);
+        
+        if (debugMode || SecondDisplayFound)
+        {
+            if (SecondDisplayFound && !Display.displays[1].active) Display.displays[1].Activate();
+            blueCamera.targetDisplay = 1;
+            blueCamera.rect = new Rect(0f, 0, 1, 1);
+            redCamera.rect = new Rect(0, 0, 1, 1);
+        }
+        else
+        {
+            blueCamera.targetDisplay = 0;
+            blueCamera.rect = new Rect(0.5f, 0, 0.5f, 1);
+            redCamera.rect = new Rect(0, 0, 0.5f, 1);
+        }
     }
 }
