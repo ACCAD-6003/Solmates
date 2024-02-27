1fdc681ffe3edf2a9d48f3f9cbbbfb64261318e2
Chaseao <oberg.17@buckeyemail.osu.edu>
2024-02-27 10:02:00 -0500

    Added door animations

--- Assets/Scripts/Doors/DoorController.cs
+++ Assets/Scripts/Doors/DoorController.cs
@@ -1,15 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class DoorController : MonoBehaviour
 {
+    private static readonly int VerticalOffset = Shader.PropertyToID("_Vertical_Offset");
+    
     [SerializeField] bool requiresBothButtons = false;
     [SerializeField] bool doorStayOpen = false;
     [SerializeField] bool doorIsOpen = false;
 
+    [Header("Door Animation Values")] 
+    [SerializeField] private bool animates;
+    [SerializeField, ShowIf("animates")] private MeshRenderer doorRenderer;
+    [SerializeField, ShowIf("animates")] private Collider doorCollider;
+    [SerializeField, ShowIf("animates")] private float doorOpenAmount = 4f;
+    [SerializeField, ShowIf("animates")] private float doorClosedAmount = 10f;
+    [SerializeField, ShowIf("animates")] private float changeRate = 1f;
+    
     public ButtonController[] buttons;
+    private bool DoorIsOpen {
+        get => doorIsOpen;
+        set
+        {
+            doorIsOpen = value;
+            UpdateAnimator();
+        }
+    }
+    
+    private void OnEnable()
+    {
+        if (animates)
+        {
+            doorRenderer.material.SetFloat(VerticalOffset, doorIsOpen ? doorOpenAmount : doorClosedAmount);
+            
+            StartCoroutine(HandleDoorAnimation());
+        }
+    }
 
+    private void UpdateAnimator()
+    {
+        if (!animates)
+        {
+            gameObject.SetActive(!DoorIsOpen);
+        }
+    }
+
+    private IEnumerator HandleDoorAnimation()
+    {
+        var currentAmount = doorIsOpen ? doorOpenAmount : doorClosedAmount;
+        while (true)
+        {
+            Debug.Log("Strating");
+            doorCollider.enabled = doorCollider.enabled || !doorIsOpen;
+            while (Mathf.Abs(currentAmount - (doorIsOpen ? doorOpenAmount : doorClosedAmount)) > 0.01f)
+            {
+                currentAmount += doorIsOpen ? changeRate * Time.deltaTime : -changeRate * Time.deltaTime;
+                Debug.Log(currentAmount);
+                currentAmount = Mathf.Clamp(currentAmount, doorClosedAmount, doorOpenAmount);
+                doorRenderer.material.SetFloat(VerticalOffset, currentAmount);
+                yield return null;
+            }
+
+            var lastValue = doorIsOpen;
+            doorCollider.enabled = !doorIsOpen;
+            yield return new WaitUntil(() => doorIsOpen != lastValue);
+        }
+    }
+    
     public void CheckIfDoorShouldOpen()
     {
         bool allButtonsPressed = true;
@@ -19,8 +80,7 @@ public class DoorController : MonoBehaviour
             {
                 if (!requiresBothButtons)
                 {
-                    doorIsOpen = true;
-                    gameObject.SetActive(false);
+                    DoorIsOpen = true;
                 }
             }
             else
@@ -34,8 +94,7 @@ public class DoorController : MonoBehaviour
 
         if (allButtonsPressed && requiresBothButtons)
         {
-            doorIsOpen = true;
-            gameObject.SetActive(false);
+            DoorIsOpen = true;
         }
     }
 
@@ -45,16 +104,14 @@ public class DoorController : MonoBehaviour
         {
             if (button.isPressed)
             {
-                doorIsOpen = true;
-                gameObject.SetActive(false);
+                DoorIsOpen = true;
                 break;
             }
             else
             {
-                if (!doorStayOpen && doorIsOpen)
+                if (!doorStayOpen && DoorIsOpen)
                 {
-                    doorIsOpen = false;
-                    gameObject.SetActive(true);
+                    DoorIsOpen = false;
                 }
             }
         }
